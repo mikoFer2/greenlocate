508861114f0109e3569ea5ee23934f47b4cd5cc1
michael <xgnrpro@gmail.com>
2024-06-08 16:51:39 -0400

    version 1.0.0

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import { pool } from './db.js'
+import {PORT} from './config.js'
+import cors from 'cors'
+
+import bodyParser from 'body-parser' //obligatario para parsear datos, sino da problemas de sintaxis sql
+
+const app = express()
+
+// Permitir solicitudes desde todos los dominios
+app.use(cors());
+app.use(bodyParser.json());
+
+//acá se crean las acciones a bbdd
+app.get('/get_data_area_verde', async (req, res) => {
+  const [rows] = await pool.query('SELECT * FROM area_verde')
+  res.json(rows)
+})
+
+app.post('/get_pass', async (req, res) => {
+    const { user } = req.body
+    const INSERT_QUERY = 'SELECT Contrasena FROM usuario WHERE rut = ?';
+    const [rows] = await pool.query(INSERT_QUERY, [user])
+    res.json(rows)
+  })
+
+app.get('/get_data_usuarios', async (req, res) => {
+    const [rows] = await pool.query('SELECT * FROM usuario')
+    res.json(rows)
+  })
+
+app.get('/show_tables', async (req, res) => {
+  const result = await pool.query('SHOW TABLES')
+  res.json(result)
+})
+
+//prueba de conexion
+app.get('/ping', async (req, res) => {
+  const [result] = await pool.query(`SELECT "hello world" as RESULT`);
+  res.json(result[0])
+})
+
+app.listen(PORT)
+console.log('Server on port', PORT)
---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 3000
+
+export const DB_HOST = process.env.DB_HOST || 'monorail.proxy.rlwy.net'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'BRGjtmOMnkpTIvXubghoJMnUaIPQYmWN'
+export const DB_NAME = process.env.DB_NAME || 'greenlocate'
+export const DB_PORT = process.env.DB_PORT || 11450

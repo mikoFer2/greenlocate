dd7a22eb0d1fd84b6fd66eb1495614b5536e9f95
michael <xgnrpro@gmail.com>
2024-06-08 19:25:16 -0400

    version 1.0.3

--- greenlocate-fe/src/app/pages/login/login.page.ts
+++ greenlocate-fe/src/app/pages/login/login.page.ts
@@ -1,31 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { MapControllerService } from 'src/app/services/mapcontroller.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  username!: string;
+  password!: string;
+  pass!: string;
 
   mdl_rut: string = '';
   mdl_contrasena: string = '';
   
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private taskService: MapControllerService,
+    private alertController: AlertController
+  ) 
+  { }
 
-  ngOnInit() {
-  }
+  async login() {
+    const response = await this.taskService.getPass(this.username );
+    console.log("mi response: ", response.Contrasena)
+    
+    //falta validar si existe usuario y el manejo de errores
+    if (this.password === response.Contrasena) {
+      this.router.navigate(['/inicio']);
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Login Failed',
+        message: 'Invalid username or password.',
+        buttons: ['OK']
+      });
 
-  ingresar(){
-    let extras: NavigationExtras = {
-      replaceUrl: true,
-      state: {
-        rut: this.mdl_rut,
-        contrasena: this.mdl_contrasena
-      }
+      await alert.present();
     }
-
-    this.router.navigate(['inicio'], extras)
   }
 
   crearusuario(){
--- greenlocate-fe/src/app/services/mapcontroller.service.ts
+++ greenlocate-fe/src/app/services/mapcontroller.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MapcontrollerService {
+
+export class MapControllerService {
+  private baseUrl = 'http://localhost:3000';
 
   constructor() { }
+
+  async getTables() {
+    const response = await axios.get(`${this.baseUrl}/show_tables`);
+    return response.data[0];
+  }
+
+  async getPass(user: string) {
+    const response = await axios.post(`${this.baseUrl}/get_pass`, { user });
+    console.log('recibo: ', response);
+    return response.data[0];
+  }
+
+  async getTasks() {
+    const response = await axios.get(`${this.baseUrl}/show_tables`);
+    return response.data;
+  }
+
+  async getTest() {
+    const response = await axios.get(`${this.baseUrl}`);
+    return response.data;
+  }
+
+  async addTask(task: string) {
+    const response = await axios.post(`${this.baseUrl}/tasks`, { task });
+    return response.data;
+  }
 }

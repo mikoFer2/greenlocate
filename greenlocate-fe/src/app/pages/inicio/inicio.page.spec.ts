714b7183f750af2b73f73b32f20a9ca97a874b88
lae89 <edgar_xz10@hotmail.com>
2024-06-21 15:48:19 -0400

    4

--- greenlocate-fe/src/app/pages/inicio/inicio.page.spec.ts
+++ greenlocate-fe/src/app/pages/inicio/inicio.page.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { InicioPage } from './inicio.page';
+import { InicioPage } from './InicioPage';
 
 describe('InicioPage', () => {
   let component: InicioPage;
--- greenlocate-fe/src/app/pages/inicio/inicio.page.ts
+++ greenlocate-fe/src/app/pages/inicio/inicio.page.ts
@@ -1,49 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
-//declare var google;
+declare var google: any;
+
+
+import { Component, ElementRef, OnInit, Renderer2, ViewChild, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { MapComponent } from './map/inicio.page';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.page.html',
-  styleUrls: ['./inicio.page.scss'],
+    selector: 'app-inicio',
+    templateUrl: './inicio.page.html',
+    styleUrls: ['./inicio.page.scss'],
+    standalone: true,
+    imports: [MapComponent],
 })
 export class InicioPage implements OnInit {
 
-  rut: string = '';
-  contrasena: string = '';
+    @ViewChild('map', { static: true }) mapElementRef!: ElementRef;
+    center = { lng: -70.745728, lat: -33.54056};
+    googleMaps: any;
+    map: any;
+    marker: any;
+    mapListener: any;
+    markerListener: any;
+    intersectionObserver: any;
+    private renderer = inject(Renderer2);
 
-//  map = null;
-  
-  constructor(private router: Router) { }
+    rut: string = '';
+    contrasena: string = '';
 
-  ngOnInit() {
+    constructor(private router: Router) { }
+
+    ngOnInit() {
     let extras = this.router.getCurrentNavigation();
 
-    if(extras?.extras.state){
-      this.rut = extras?.extras.state['rut'];
-      this.contrasena = extras?.extras.state['contrasena'];
+    if (extras?.extras.state) {
+        this.rut = extras?.extras.state['rut'];
+        this.contrasena = extras?.extras.state['contrasena'];
     }
-    
-  }
 
-//  loadMap() {
-//    const mapEle: HTMLElement = document.getElementById('map');
+    }
 
-//    const myLatLng = {
-//      lat: this.marker.position.lat,
-//      lng: this.marker.position.lng
-//    };
+    async loadMap(){
+        const { Map } = await google.maps.importLibrary("maps");
 
-//    this.map = new google.maps.Map(mapEle, {
-//      center: myLatLng,
-//      zoom: 15
-//    });
-  
-//    google.maps.event.addListenerOnce(this.map, 'idle', () => {
-//      this.addMarker(this.marker);
-//      mapEle.classList.add('show-map');
-//    });
-//  }
+        const mapEl = this.mapElementRef.nativeElement;
 
+        const location = new google.maps.LatLng(this.center.lat, this.center.lng);
+
+        this.map = new Map(mapEl, {
+            center: location,
+            zoom: 14,
+            mapId: "4504f8b37365c3d0",
+        });
+
+        this.renderer.addClass(mapEl, 'visible');
+    }
 }
